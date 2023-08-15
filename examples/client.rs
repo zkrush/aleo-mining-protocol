e007e01485fe1a3d2e2999b817988fefff0004c1
ghostant-1017 <syyanyuhui@qq.com>
2023-08-15 14:42:15 +0800

    Add examples

---
+++ examples/client.rs
@@ -0,0 +1,48 @@
+use aleo_mining_protocol::{PoolMessage, AuthRequest};
+use futures_util::{StreamExt, SinkExt, stream::{SplitStream, SplitSink}};
+use log::info;
+use snarkvm_console_network::{Testnet3, Network};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream, tungstenite::Message};
+use anyhow::Result;
+#[tokio::main]
+async fn main() -> Result<()>{
+    // std::env::set_var("RUST_LOG", "INFO");
+    env_logger::init();
+    let endpoint = url::Url::parse("ws://127.0.0.1:3030")?;
+    let (ws, _) = connect_async(endpoint).await?;
+    let (mut outgoing, mut incoming) = ws.split();
+    
+
+    // 1.Prepare auth message
+    let message: PoolMessage<Testnet3> = PoolMessage::AuthRequest(AuthRequest{
+        username: "test123".to_string(),
+    });
+    send_frame(&mut outgoing, message).await?;
+
+    // 2. Read auth response
+    let message: PoolMessage<Testnet3> = read_frame(&mut incoming).await?;
+    let auth_response = message.auth_response().unwrap();
+    info!("[aleo-mining-protocol] receive auth response: {:?}", auth_response);
+
+    // 3. Read new task
+    let message: PoolMessage<Testnet3> = read_frame(&mut incoming).await?;
+    let new_task = message.new_task().unwrap();
+    info!("[aleo-mining-protocol] receive auth response: {:?}", new_task);
+
+    // 4. Prove and submit solution
+
+    Ok(())
+}
+
+pub async fn send_frame<N: Network>(outgoing: &mut SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>, message: PoolMessage<N>) -> Result<()>{
+    let ws_message = tokio_tungstenite::tungstenite::Message::text(serde_json::to_string(&message)?);
+    outgoing.send(ws_message).await?;
+    Ok(())
+}
+
+pub async fn read_frame<N: Network>(incoming: &mut SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) -> Result<PoolMessage<N>> {
+    let ws_message = incoming.next().await.unwrap()?;
+    let message: PoolMessage<N> = serde_json::from_str(&ws_message.to_string())?;
+    Ok(message)
+}
---
+++ examples/server.rs
@@ -0,0 +1,78 @@
+use std::{net::SocketAddr, sync::Arc, env};
+
+use futures_util::{StreamExt, SinkExt, stream::{SplitSink, SplitStream}};
+use serde_json::json;
+use snarkvm_console_network::{Network, Testnet3};
+use snarkvm_console_types_address::{Address, FromStr};
+use snarkvm_ledger_coinbase::EpochChallenge;
+use tokio::net::{TcpListener, TcpStream};
+use log::*;
+use tokio_tungstenite::{accept_async, MaybeTlsStream, WebSocketStream, tungstenite::Message};
+use anyhow::{Result, ensure};
+use aleo_mining_protocol::{PoolMessage, AuthResponse, NewTask};
+pub struct PoolState<N: Network> {
+    address: Address<N>,
+    epoch_challenge: EpochChallenge<N>,
+}
+
+#[tokio::main]
+async fn main() {
+    env::set_var("RUST_LOG", "INFO");
+    env_logger::init();
+    let addr = "127.0.0.1:3030";
+    let address = Address::from_str("aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px").unwrap();
+    let epoch_challenge = EpochChallenge::new(0, Default::default(), (1 << 12) - 1).unwrap();
+    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+    let state = Arc::new(PoolState{address, epoch_challenge});
+    info!("Listening on: {}", addr);
+    while let Ok((stream, peer)) = listener.accept().await {
+        let state = state.clone();
+        tokio::spawn(async move {
+            if let Err(err) = handle_connection::<Testnet3>(stream, peer, state).await {
+                error!("Handle connection error: {err}");
+            }
+        });
+    }
+}
+
+async fn handle_connection<N: Network>(stream: TcpStream, peer: SocketAddr, state: Arc<PoolState<N>>) -> Result<()> {
+    let ws_stream = accept_async(stream).await.expect("Failed to accept");
+    info!("New websocket client: {}", peer);
+    let (mut outgoing, mut incoming) = ws_stream.split();
+    // 1. Wait for auth request
+    let message: PoolMessage<N> = read_frame(&mut incoming).await?;
+    ensure!(matches!(message, PoolMessage::AuthRequest(..)));
+    let _ = message.auth_request().unwrap();
+
+    // 2. Send auth response
+    let message = PoolMessage::AuthResponse(AuthResponse {
+        result: true,
+        address: state.address.clone(),
+        message: None,
+    });
+    send_frame(&mut outgoing, message).await?;
+    // 3. Notify current epoch challenge
+    let message = PoolMessage::NewTask(NewTask{
+        epoch_challenge: state.epoch_challenge.clone(),
+        difficulty: 0,
+    });
+    send_frame(&mut outgoing, message).await?;
+
+    // 4. Receive solution
+    let message: PoolMessage<N> = read_frame(&mut incoming).await?;
+    matches!(message, PoolMessage::NewSolution(..));
+    Ok(())
+}
+
+
+pub async fn send_frame<N: Network>(outgoing: &mut SplitSink<WebSocketStream<TcpStream>, Message>, message: PoolMessage<N>) -> Result<()>{
+    let ws_message = tokio_tungstenite::tungstenite::Message::text(serde_json::to_string(&message)?);
+    outgoing.send(ws_message).await?;
+    Ok(())
+}
+
+pub async fn read_frame<N: Network>(incoming: &mut SplitStream<WebSocketStream<TcpStream>>) -> Result<PoolMessage<N>> {
+    let ws_message = incoming.next().await.unwrap()?;
+    let message: PoolMessage<N> = serde_json::from_str(&ws_message.to_string())?;
+    Ok(message)
+}
