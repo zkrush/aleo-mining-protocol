26087c86eefdf0cfeaefba3d10e04ba09acdc801
ghostant-1017 <syyanyuhui@qq.com>
2023-12-20 11:35:39 +0800

    Add pubsub

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 pub mod client;
 #[cfg(feature = "client")]
 mod connection;
-
+#[cfg(feature = "pubsub")]
+pub mod pubsub;
 use serde::ser::SerializeStruct;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
---
+++ src/pubsub.rs
@@ -0,0 +1,70 @@
+use std::sync::Arc;
+
+use snarkvm::prelude::Network;
+use tokio::sync::Semaphore;
+use crate::NewTask;
+
+pub struct PubSub<N: Network> {
+    limit: Arc<tokio::sync::Semaphore>,
+    task_pub: tokio::sync::broadcast::Sender<Arc<NewTask<N>>>,
+    current_task: Option<Arc<NewTask<N>>>,
+}
+
+impl<N: Network> PubSub<N> {
+    pub fn new(limit: usize) -> Self {
+        let limit = Arc::new(Semaphore::new(limit as usize));
+        let current_task = None;
+        let (task_pub, _) = tokio::sync::broadcast::channel(1);
+        Self {
+            limit,
+            task_pub,
+            current_task,
+        }
+    }
+
+    pub fn update_task(&mut self, task: NewTask<N>) {
+        let task = Arc::new(task);
+        self.current_task = Some(task.clone());
+
+        // Only fail if there are no subs, so we just ignore the error
+        let _ = self.task_pub.send(task.clone());
+    }
+
+    pub fn add_sub(&self) -> anyhow::Result<tokio::sync::mpsc::Receiver<Arc<NewTask<N>>>> {
+        // Ensure we dont have too many subs
+        let _permit = self.limit.clone().try_acquire_owned()?;
+        let mut task_sub = self.task_pub.subscribe();
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let mut current_task = self.current_task.clone();
+        tokio::spawn(async move {
+            // Move the permit into the task, so that it is released when the task is dropped
+            let _permit = _permit;
+            // 1. If we have task now, send it to the new subscriber
+            if let Some(task) = current_task.take() {
+                // Ignore the error if the sub has gone
+                if let Err(_) = tx.send(task).await {
+                    return;
+                }
+            }
+            // 2. Send the task to the sub when we `update_task`
+            loop {
+                match task_sub.recv().await {
+                    Ok(task) => {
+                        // Break the loop if the sub has gone
+                        if let Err(_) = tx.send(task).await {
+                            break;
+                        }
+                    },
+                    Err(tokio::sync::broadcast::error::RecvError::Closed) => {
+                        break;
+                    },
+                    // Continue so that we will recv the latest task
+                    Err(tokio::sync::broadcast::error::RecvError::Lagged(_)) => {
+                        continue;
+                    }
+                }
+            }
+        });
+        Ok(rx)
+    }
+}
